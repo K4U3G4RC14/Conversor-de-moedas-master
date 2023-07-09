e5cc4066d4b40a4df961dad8113352bfb80422f7
K4U3G4RC14 <kauegarcia666@gmai.com>
2023-07-09 15:26:57 -0300

    exclusão de comentarios

--- script.js
+++ script.js
@@ -10,47 +10,14 @@ const convet_value = async () => {
     const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(response => response.json())
 
     const dolar = data.USDBRL.high
-    const euro =  data.EURBRL.high
-    const cripto =  data.BTCBRL.high
+    const euro = data.EURBRL.high
+    const cripto = data.BTCBRL.high
 
     text_real.innerHTML = new Intl.NumberFormat('pt-br',
         { style: 'currency', currency: 'brl' }
     ).format(real_value);
 
-    /*
-    switch (real_value) {
-        case "":
-            if(real_value === ""){
-                alert("Você precisa inserrir um valor antes")
-            }
-            break;
-        case real_value:
-            btn.setAttribute("disabled", false)
-            if (select.value === "US$ Dolar Americano") {
-                other_money.innerHTML = new Intl.NumberFormat('pt-US',
-                    { style: 'currency', currency: 'USD' }
-                ).format(real_value / dolar);
-            }
-        
-            if (select.value === "€ Euro") {
-                other_money.innerHTML = new Intl.NumberFormat('pt-DE',
-                    { style: 'currency', currency: 'EUR' }
-                ).format(real_value / euro);
-            }
-        
-            if (select.value === "BTC Bitcoin") {
-                other_money.innerHTML = new Intl.NumberFormat('pt-BR',
-                    { style: 'currency', currency: 'BTC' }
-                ).format(real_value / cripto);
-            }
-            break
-            default:
-            break;
-    }
-    */
-
-
-    if(real_value === ""){
+    if (real_value === "") {
         alert("Você precisa inserir um valor antes")
     }
 
@@ -71,7 +38,7 @@ const convet_value = async () => {
             { style: 'currency', currency: 'BTC' }
         ).format(real_value / cripto);
     }
-    
+
 }
 
 const change_currency = () => {
@@ -88,7 +55,7 @@ const change_currency = () => {
         currency_text.innerHTML = "Euro"
         flag.src = "./assets/Design sem nome 1.svg"
     }
-    
+
     if (select.value === "BTC Bitcoin") {
         currency_text.innerHTML = "BTC"
         flag.src = "./assets/bitcoin.png"
